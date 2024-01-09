acf6c05b8ced059fbd5c30b02154648f4018dbbd
Sahil Latif <sahillatif072@gmail.com>
2024-01-09 14:19:46 +0500

    Added cpp files
    Created cpp file and added colors definition.

---
+++ typingSpeed.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<string>
+
+using namespace std;
+
+#define cg "\033[1;32m"
+#define cy "\033[1;33m"
+#define cr "\033[1;31m"
+#define cb "\033[1;34m"
+#define cm "\033[1;35m"
+#define cw "\033[1;37m"
+#define cc "\033[1;36m"
+#define cn "\033[1;0m"
+int main(){
+    cout<<cg<<"Hello"<<cn<<"Good"<<cr<<"Very"<<cn<<"Good";
+}
